bb533ac2aee819df252f8c70512c032b03fe2360
Miguel Ponce de Leon <git@miguelpdl.com>
2021-03-13 23:23:53 +0000

    Modified to host the comparison between Tottenham and Arsenal.

--- 15PointsMovingAverage.py
+++ 15PointsMovingAverage.py
@@ -5,12 +5,13 @@ Created on Fri Mar 12 05:53:35 2021
 @author: davsu428
 """
 
-import datetime 
+import datetime
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 dflist=[]
+goaldiff = 0
 
 seasonst=[]
 for year in range(5,21,1):
@@ -23,16 +24,16 @@ for year in range(5,21,1):
     else:
         yeartext=str(year)+str(year+1)
         yeartext2=str(year)+'-'+str(year+1)
-        
+
     seasonst=seasonst+[yeartext2]
     #For England
-    performance_year = pd.read_csv("https://www.football-data.co.uk/mmz4281/"+yeartext+"/E0.csv",delimiter=',') 
+    performance_year = pd.read_csv("https://www.football-data.co.uk/mmz4281/"+yeartext+"/E0.csv",delimiter=',')
     #For Spain
-    #performance_year = pd.read_csv("https://www.football-data.co.uk/mmz4281/"+yeartext+"/SP1.csv",delimiter=',') 
+    #performance_year = pd.read_csv("https://www.football-data.co.uk/mmz4281/"+yeartext+"/SP1.csv",delimiter=',')
     dflist=dflist+[performance_year]
 
 performance= pd.concat(dflist)
-        
+
 teams=list(set(performance['AwayTeam']))
 
 
@@ -46,9 +47,12 @@ for team in teams:
     matches=performance[((performance['AwayTeam']==team) | (performance['HomeTeam']==team))]
     for i,match in matches.iterrows():
         game=game+1
-            
-        #matchdate = datetime.datetime.strptime(match['Date'],"%d/%m/%y")
-        
+
+        if len(match['Date']) <= 8:
+            matchdate = datetime.datetime.strptime(match['Date'],"%d/%m/%y")
+        else:
+            matchdate = datetime.datetime.strptime(match['Date'],"%d/%m/%Y")
+
         if match['AwayTeam']==team:
             goalsfor=match['FTAG']
             goalsagainst=match['FTHG']
@@ -76,11 +80,11 @@ for team in teams:
                 points=0
                 profit=-1
         goaldiff=goalsfor-goalsagainst
-        rows.append([matchdate,goalsfor,goalsagainst,goaldiff,points,profit,season,game])
-    
+        rows.append([matchdate,goalsfor,goalsagainst,goaldiff,points,profit,seasonst,game])
+
     ma_goaldiff=np.convolve(goaldiff, np.ones(10)/10, mode='valid')
-    
-    df = pd.DataFrame(rows, columns=["Date", "For","Against","Difference","Points","Profit","Season","Game"])    
+
+    df = pd.DataFrame(rows, columns=["Date", "For","Against","Difference","Points","Profit","Season","Game"])
     #df = df.sort_values('Date', ascending=True)
     df['PointsRA'] = df['Points'].rolling(window=ma, win_type='triang').mean()
     teamdf[team]=df
@@ -93,12 +97,12 @@ import matplotlib.dates as mdates
 
 fig,ax=plt.subplots(num=1)
 
-comparison1='Man City'
-comp_color1='lightblue'
-comparison3='Liverpool'
-comp_color3='red'
-comparison2='Man United'
-comp_color2='darkred'
+comparison1='Tottenham'
+comp_color1='blue'
+comparison2='Arsenal'
+comp_color2='red'
+#comparison3='Man United'
+#comp_color3='darkred'
 
 #
 #comparison1='Real Madrid'
@@ -110,12 +114,12 @@ comp_color2='darkred'
 
 ax.plot(teamdf[comparison1]['Game'],  teamdf[comparison1]['PointsRA'], linewidth=2, linestyle='-',color=comp_color1)
 ax.plot(teamdf[comparison2]['Game'],  teamdf[comparison2]['PointsRA'], linewidth=2 , linestyle='-',color=comp_color2)
-ax.plot(teamdf[comparison3]['Game'],  teamdf[comparison3]['PointsRA'], linewidth=2 , linestyle='-',color=comp_color3)
+#ax.plot(teamdf[comparison3]['Game'],  teamdf[comparison3]['PointsRA'], linewidth=2 , linestyle='-',color=comp_color3)
 
 ax.set_title(str(ma) + ' game moving average')
 plt.gcf().autofmt_xdate()
 
-ax.legend([comparison1,comparison2,comparison3])
+ax.legend([comparison1,comparison2])
 
 ax.set_ylim(1,3.2)
 
@@ -132,4 +136,4 @@ ax.set_ylabel('Rolling Average Points Per Game')
 ax.set_xlabel('Season')
 
 
-fig.savefig('dip.png', dpi=None, bbox_inches="tight")
+fig.savefig('Output/dip_totars.png', dpi=None, bbox_inches="tight")
